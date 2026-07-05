4ecbd6c5531f12a7c1a1902530c4dca4f26fc4d9
kavay khurana <kavaykhurana4@gmail.com>
2026-07-05 23:54:07 +0530

    perf(repository): add EntityGraph to optimize Comment author fetch

--- src/main/java/com/blog/repository/CommentRepository.java
+++ src/main/java/com/blog/repository/CommentRepository.java
@@ -4,10 +4,12 @@ import com.blog.model.Comment;
 import com.blog.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @EntityGraph(attributePaths = {"author"})
     Page<Comment> findByPostAndParentIsNull(Post post, Pageable pageable);
 }
